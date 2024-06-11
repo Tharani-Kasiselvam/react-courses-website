9b85ddd38c7eb0141ede4f69ea94f8a9f6a09b6b
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-06-11 23:27:14 +0530

    fixed navbar toggle issue

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -31,50 +31,52 @@ const Courses = () => {
         <div>
             <CoursesContext.Provider value={{ selectedCourse }}>
                 <div className="nav-header">
-                    <nav className="navbar navbar-expand-lg navbar-light">
-                        <Link className="navbar-brand" to="/courses/All" onClick={(onNavLinkClick)}>Courses</Link>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
+                    <div className="container-fluid">
+                        <nav className="navbar navbar-expand-lg navbar-light">
+                            <Link className="navbar-brand" to="/courses/All" onClick={(onNavLinkClick)}>Courses</Link>
+                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
 
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav mr-auto" id="nav-all">
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/courses/All"
-                                        style={isActive => ({
-                                            color: isActive ? "black" : "gray"
-                                        })} onClick={(onNavLinkClick)}>All</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/courses/FullStackDevelopment"
-                                        style={isActive => ({
-                                            color: isActive ? "black" : "gray"
-                                        })} onClick={(onNavLinkClick)}>Full Stack Development</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/courses/DataScience"
-                                        style={isActive => ({
-                                            color: isActive ? "black" : "gray"
-                                        })} onClick={(onNavLinkClick)}>Data Science</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/courses/CyberSecurity"
-                                        style={isActive => ({
-                                            color: isActive ? "black" : "gray"
-                                        })} onClick={(onNavLinkClick)}>Cyber Security</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/courses/Careers"
-                                        style={isActive => ({
-                                            color: isActive ? "black" : "gray"
-                                        })} onClick={(onNavLinkClick)}>Careers</NavLink>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
-                <div>
-                    <Outlet />
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav" id="nav-all">
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/courses/All"
+                                            style={isActive => ({
+                                                color: isActive ? "black" : "gray"
+                                            })} onClick={(onNavLinkClick)}>All</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/courses/FullStackDevelopment"
+                                            style={isActive => ({
+                                                color: isActive ? "black" : "gray"
+                                            })} onClick={(onNavLinkClick)}>Full Stack Development</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/courses/DataScience"
+                                            style={isActive => ({
+                                                color: isActive ? "black" : "gray"
+                                            })} onClick={(onNavLinkClick)}>Data Science</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/courses/CyberSecurity"
+                                            style={isActive => ({
+                                                color: isActive ? "black" : "gray"
+                                            })} onClick={(onNavLinkClick)}>Cyber Security</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/courses/Careers"
+                                            style={isActive => ({
+                                                color: isActive ? "black" : "gray"
+                                            })} onClick={(onNavLinkClick)}>Careers</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                    <div>
+                        <Outlet />
+                    </div>
                 </div>
             </CoursesContext.Provider>
         </div>
